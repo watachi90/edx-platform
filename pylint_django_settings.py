91591489cbc3899125b2c07b3eb47a6bca4988ee
UsamaSadiq <usama7274@gmail.com>
2022-10-04 20:22:29 +0500

    fix: fix pep8 failures

--- pylint_django_settings.py
+++ pylint_django_settings.py
@@ -54,5 +54,6 @@ class SetDjangoSettingsChecker(BaseChecker):
             # pylint command arguments than test setting of lms is used
             name_checker.config.django_settings_module = 'lms.envs.test'
 
+
 def register(linter):
     linter.register_checker(SetDjangoSettingsChecker(linter))
